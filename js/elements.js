fa2a70e459789ade1d2ea93f11ad5cf3c787b3ee
flexult <touma.leandre.72@gmail.com>
2025-11-19 20:05:15 +0000

    ajout dlune lumiere

--- js/elements.js
+++ js/elements.js
@@ -1,12 +1,26 @@
+// Light :
+export function createLight(scene) {
+    const light = new BABYLON.HemisphericalLight(
+        "light",
+        new BABYLON.Vector3(0, 1, 0),
+        scene
+    )
+    light.intensity = 0.7;
+    return light;
+}
+
+// Ground :
 export function createGround(scene) {
     
 }
 
+// Player :
 export function createPlayer(scene) {
     const player = new BABYLON.MeshBuilder.CreateBox("player", {size: 2}, scene);
     return player;
 }
 
+// Camera:
 export function createCamera(scene, player) {
     const camera = new BABYLON.ArcRotateCamera(
         "camera",
